9b04fdb81687ab8c94adfb1f3d86f4abfb6e2208
Lee Shiueh <lee.shiueh@gmail.com>
2023-12-30 20:37:47 +0800

    Update cwa domain model

--- src/cwagent/domain/model.py
+++ src/cwagent/domain/model.py
@@ -1,80 +1,340 @@
-from marshmallow import Schema, fields
+from dataclasses import dataclass
+
+from marshmallow import Schema, fields, post_load
 
 
-class Coordinate(Schema):
+@dataclass
+class Coordinate:
+    CoordinateName: str
+    CoordinateFormat: str
+    StationLatitude: float
+    StationLongitude: float
+
+    @classmethod
+    def load(cls, data: dict):
+        _schema = CoordinateSchema()
+        return _schema.load(data)
+
+    def dump(self):
+        _schema = CoordinateSchema()
+        return _schema.dump(self)
+
+
+class CoordinateSchema(Schema):
     CoordinateName = fields.Str()
     CoordinateFormat = fields.Str()
     StationLatitude = fields.Float()
     StationLongitude = fields.Float()
 
+    @post_load
+    def make_coordinate(self, data, **kwargs):
+        return Coordinate(**data)
 
-class GeoInfo(Schema):
-    Coordinates = fields.List(fields.Nested(Coordinate()))
+
+@dataclass
+class GeoInfo:
+    Coordinates: list
+    StationAltitude: str
+    CountyName: str
+    TownName: str
+    CountyCode: str
+    TownCode: str
+
+    @classmethod
+    def load(cls, data: dict):
+        _schema = GeoInfoSchema()
+        return _schema.load(data)
+
+    def dump(self):
+        _schema = GeoInfoSchema()
+        return _schema.dump(self)
+
+
+class GeoInfoSchema(Schema):
+    Coordinates = fields.List(fields.Nested(CoordinateSchema()))
     StationAltitude = fields.Str()
     CountyName = fields.Str()
     TownName = fields.Str()
     CountyCode = fields.Str()
     TownCode = fields.Str()
 
+    @post_load
+    def make_geo_info(self, data, **kwargs):
+        return GeoInfo(**data)
 
-class GustInfoOccurredAt(Schema):
+
+@dataclass
+class GustOccurredAt:
+    WindDirection: int
+    DateTime: str
+
+    @classmethod
+    def load(cls, data: dict):
+        _schema = GustOccurredAtSchema()
+        return _schema.load(data)
+
+    def dump(self):
+        _schema = GustOccurredAtSchema()
+        return _schema.dump(self)
+
+
+class GustOccurredAtSchema(Schema):
     WindDirection = fields.Int()
     DateTime = fields.Str()
 
+    @post_load
+    def make_gust_occurred_at(self, data, **kwargs):
+        return GustOccurredAt(**data)
 
-class GustInfo(Schema):
+
+@dataclass
+class GustInfo:
+    PeakGustSpeed: float
+    Occurred_at: GustOccurredAt
+
+    @classmethod
+    def load(cls, data: dict):
+        _schema = GustInfoSchema()
+        return _schema.load(data)
+
+    def dump(self):
+        _schema = GustInfoSchema()
+        return _schema.dump(self)
+
+
+class GustInfoSchema(Schema):
     PeakGustSpeed = fields.Float()
-    Occurred_at = fields.Nested(GustInfoOccurredAt())
+    Occurred_at = fields.Nested(GustOccurredAtSchema())
+
+    @post_load
+    def make_gust_info(self, data, **kwargs):
+        return GustInfo(**data)
 
 
-class DailyInfoOccurredAt(Schema):
+@dataclass
+class DailyInfoOccurredAt:
+    DateTime: str
+
+    @classmethod
+    def load(cls, data: dict):
+        _schema = DailyInfoOccurredAtSchema()
+        return _schema.load(data)
+
+    def dump(self):
+        _schema = DailyInfoOccurredAtSchema()
+        return _schema.dump(self)
+
+
+class DailyInfoOccurredAtSchema(Schema):
     DateTime = fields.Str()
 
+    @post_load
+    def make_daily_info_occurred_at(self, data, **kwargs):
+        return DailyInfoOccurredAt(**data)
 
-class TemperatureInfo(Schema):
+
+@dataclass
+class TemperatureInfo:
+    AirTemperature: float
+    Occurred_at: DailyInfoOccurredAt
+
+    @classmethod
+    def load(cls, data: dict):
+        _schema = TemperatureInfoSchema()
+        return _schema.load(data)
+
+    def dump(self):
+        _schema = TemperatureInfoSchema()
+        return _schema.dump(self)
+
+
+class TemperatureInfoSchema(Schema):
     AirTemperature = fields.Float()
-    Occurred_at = fields.Nested(DailyInfoOccurredAt())
+    Occurred_at = fields.Nested(DailyInfoOccurredAtSchema())
+
+    @post_load
+    def make_temperature_info(self, data, **kwargs):
+        return TemperatureInfo(**data)
 
 
-class DailyHigh(Schema):
-    TemperatureInfo = fields.Nested(TemperatureInfo())
+@dataclass
+class DailyHigh:
+    TemperatureInfo: TemperatureInfo
+
+    @classmethod
+    def load(cls, data: dict):
+        _schema = DailyHighSchema()
+        return _schema.load(data)
+
+    def dump(self):
+        _schema = DailyHighSchema()
+        return _schema.dump(self)
 
 
-class DailyLow(Schema):
-    TemperatureInfo = fields.Nested(TemperatureInfo())
+class DailyHighSchema(Schema):
+    TemperatureInfo = fields.Nested(TemperatureInfoSchema())
+
+    @post_load
+    def make_daily_high(self, data, **kwargs):
+        return DailyHigh(**data)
 
 
-class DailyExtreme(Schema):
-    DailyHigh = fields.Nested(DailyHigh())
-    DailyLow = fields.Nested(DailyLow())
+@dataclass
+class DailyLow:
+    TemperatureInfo: TemperatureInfo
+
+    @classmethod
+    def load(cls, data: dict):
+        _schema = DailyLowSchema()
+        return _schema.load(data)
+
+    def dump(self):
+        _schema = DailyLowSchema()
+        return _schema.dump(self)
 
 
-class Now(Schema):
+class DailyLowSchema(Schema):
+    TemperatureInfo = fields.Nested(TemperatureInfoSchema())
+
+    @post_load
+    def make_daily_low(self, data, **kwargs):
+        return DailyLow(**data)
+
+
+@dataclass
+class DailyExtreme:
+    DailyHigh: DailyHigh
+    DailyLow: DailyLow
+
+    @classmethod
+    def load(cls, data: dict):
+        _schema = DailyExtremeSchema()
+        return _schema.load(data)
+
+    def dump(self):
+        _schema = DailyExtremeSchema()
+        return _schema.dump(self)
+
+
+class DailyExtremeSchema(Schema):
+    DailyHigh = fields.Nested(DailyHighSchema())
+    DailyLow = fields.Nested(DailyLowSchema())
+
+    @post_load
+    def make_daily_extreme(self, data, **kwargs):
+        return DailyExtreme(**data)
+
+
+@dataclass
+class Now:
+    Precipitation: int  # 降雨量
+
+    @classmethod
+    def load(cls, data: dict):
+        _schema = NowSchema()
+        return _schema.load(data)
+
+    def dump(self):
+        _schema = NowSchema()
+        return _schema.dump(self)
+
+
+class NowSchema(Schema):
     Precipitation = fields.Int()
 
+    @post_load
+    def make_now(self, data, **kwargs):
+        return Now(**data)
 
-class WeatherElement(Schema):
+
+@dataclass
+class WeatherElement:
+    Weather: str
+    Now: Now
+    WindDirection: int
+    WindSpeed: float
+    AirTemperature: float
+    RelativeHumidity: int
+    AirPressure: float
+    GustInfo: GustInfo
+    DailyExtreme: DailyExtreme
+
+    @classmethod
+    def load(cls, data: dict):
+        _schema = WeatherElementSchema()
+        return _schema.load(data)
+
+    def dump(self):
+        _schema = WeatherElementSchema()
+        return _schema.dump(self)
+
+
+class WeatherElementSchema(Schema):
     Weather = fields.Str()
-    Now = fields.Nested(Now())
+    Now = fields.Nested(NowSchema())
     WindDirection = fields.Int()
     WindSpeed = fields.Float()
     AirTemperature = fields.Float()
     RelativeHumidity = fields.Int()
     AirPressure = fields.Float()
-    GustInfo = fields.Nested(GustInfo())
-    DailyExtreme = fields.Nested(DailyExtreme())
+    GustInfo = fields.Nested(GustInfoSchema())
+    DailyExtreme = fields.Nested(DailyExtremeSchema())
+
+    @post_load
+    def make_weather_element(self, data, **kwargs):
+        return WeatherElement(**data)
 
 
-class ObsTime(Schema):
+@dataclass
+class ObsTime:
+    DateTime: str
+
+    @classmethod
+    def load(cls, data: dict):
+        _schema = ObsTimeSchema()
+        return _schema.load(data)
+
+    def dump(self):
+        _schema = ObsTimeSchema()
+        return _schema.dump(self)
+
+
+class ObsTimeSchema(Schema):
     DateTime = fields.Str()
 
+    @post_load
+    def make_obs_time(self, data, **kwargs):
+        return ObsTime(**data)
 
-class Station(Schema):
+
+@dataclass
+class Station:
+    StationName: str
+    StationId: str
+    ObsTime: ObsTime
+    GeoInfo: GeoInfo
+    WeatherElement: WeatherElement
+
+    @classmethod
+    def load(cls, data: dict):
+        _schema = StationSchema()
+        return _schema.load(data)
+
+    def dump(self):
+        _schema = StationSchema()
+        return _schema.dump(self)
+
+
+class StationSchema(Schema):
     StationName = fields.Str()
     StationId = fields.Str()
-    ObsTime = fields.Nested(ObsTime())
-    GeoInfo = fields.Nested(GeoInfo())
-    WeatherElement = fields.Nested(WeatherElement())
+    ObsTime = fields.Nested(ObsTimeSchema())
+    GeoInfo = fields.Nested(GeoInfoSchema())
+    WeatherElement = fields.Nested(WeatherElementSchema())
+
+    @post_load
+    def make_station(self, data, **kwargs):
+        return Station(**data)
 
 
 station = {
@@ -143,7 +403,8 @@ station = {
 }
 
 if __name__ == '__main__':
-    # obj = Station().load(station)
-    obj = DailyExtreme().load(station["WeatherElement"]["DailyExtreme"])
-    print(type(obj))
-    # assert obj.dump() == station
+    obj = StationSchema().load(station)
+    # obj = DailyExtremeSchema().load(station["WeatherElement"]["DailyExtreme"])
+    # print(type(obj))
+    assert isinstance(obj, Station)
+    assert obj.dump() == station
